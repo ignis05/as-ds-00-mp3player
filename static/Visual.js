5b51a71e54c3b980797247c62803d4c86f6286db
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-03-08 08:57:06 +0100

    improved visualisation

--- static/Visual.js
+++ static/Visual.js
@@ -11,12 +11,27 @@ class Visual {
         data = data.split(",")
         var overlay = $("#overlay")
         $(".visualBar").remove()
-        for(let i in data){
-            let x = $("<div class='visualBar'>")
-            x.css("width", data[i])
-            x.css("left", "50%")
-            x.appendTo(overlay)
+        for (let i in data) {
+            let top = 50 + (25 * i)
+            let side = $(window).width() / 2 + 5
+            let r = 255
+            let g = 0
+            let b = data[i]
+            let color = `rgb(${r},${g},${b})`
+
+            let left = $("<div class='visualBar'>")
+            left.css("width", data[i])
+            left.css("left", side)
+            left.css("top", top)
+            left.css("background", color)
+            left.appendTo(overlay)
+
+            let right = $("<div class='visualBar'>")
+            right.css("width", data[i])
+            right.css("right", side)
+            right.css("top", top)
+            right.css("background", color)
+            right.appendTo(overlay)
         }
     }
-
 }
