60861575c51bb35c979c2c7c92e4a12edb86b87e
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-03-08 08:34:31 +0100

    added simple visual display

--- static/Visual.js
+++ static/Visual.js
@@ -7,7 +7,16 @@ class Visual {
 
     render() {
         requestAnimationFrame(this.render.bind(this)); // bind(this) przekazuje this do metody render
-        $("#overlay").html(music.getData()) // wyświetlenie danych audio w div-ie
+        var data = music.getData()
+        data = data.split(",")
+        var overlay = $("#overlay")
+        $(".visualBar").remove()
+        for(let i in data){
+            let x = $("<div class='visualBar'>")
+            x.css("width", data[i])
+            x.css("left", "50%")
+            x.appendTo(overlay)
+        }
     }
 
 }
